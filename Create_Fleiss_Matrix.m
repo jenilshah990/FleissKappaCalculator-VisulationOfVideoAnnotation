5bbade2deca56b75d7c5779d0d90a516e1e6490c
jenilshah990 <55563927+jenilshah990@users.noreply.github.com>
2020-07-11 20:03:28 +0530

    Add files via upload

---
+++ Create_Fleiss_Matrix.m
@@ -0,0 +1,12 @@
+%Create a Fleiss Kappa Matrix from the Labelling Data
+% k: Number of categories
+% matrix: Matrix of Labelled Data 
+
+function Fleiss_kappa_matrix = Create_Fleiss_Matrix(k, matrix) 
+Fleiss_kappa_matrix = zeros(size(matrix,2),k);
+for i=1:size(matrix,2)
+    for j=1:size(matrix,1)
+        Fleiss_kappa_matrix(i, matrix(j,i)) = Fleiss_kappa_matrix(i, matrix(j,i)) + 1;
+    end
+end
+end
