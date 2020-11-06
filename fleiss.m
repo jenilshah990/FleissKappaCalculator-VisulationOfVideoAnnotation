19602683d5824582458bc3a3aeda812d3835df11
Jenil Shah <55563927+jenilshah990@users.noreply.github.com>
2020-11-07 02:32:37 +0530

    Update fleiss.m
    Added Video Names & Rubric Version

--- fleiss.m
+++ fleiss.m
@@ -1,4 +1,4 @@
-% Syntax: 	fleiss(k, X, alpha)
+% Syntax: 	fleiss(k, X, alpha, label, rubric_version, video_name)
 %      
 %     Inputs:
 %           k = number of categories
@@ -7,6 +7,9 @@
 %               The (i,j) entry represents the category {1,2,3,4...} the 
 %               ith labeller put for the jth frame. 
 %           alpha = Significance Level
+%           Label = The label being labelled
+%           Rubric_version = Version of the Rubric
+%           Video_name = Name of the Video 
 %
 %     Outputs:
 %           - Visualisation of The Annotation Matrix
@@ -18,11 +21,11 @@
 %           - Kappa benchmarks by Landis and Koch 
 %           - z test & P Value
 
-
-function fleiss(k, matrix, alpha)
+function fleiss(k, matrix, alpha, label, rubric_version, video_name)
 n=size(matrix, 1); %Number of Labellers
 matrix(matrix(:) == 10) = k; %Changing 10 to highest value k
-visualisation(matrix,n); 
+visualisation(matrix,n, label, rubric_version, video_name); 
 F = Create_Fleiss_Matrix(k,matrix);
+heat_map(F,k, label, rubric_version, video_name);
 fleiss_score(F,alpha);
 end
