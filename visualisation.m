96493a43cf694ca5f184c75555ea632f5c1c1b2e
Jenil Shah <55563927+jenilshah990@users.noreply.github.com>
2020-11-07 02:34:48 +0530

    Added Video Name & rubric version + Save functionality

--- visualisation.m
+++ visualisation.m
@@ -1,10 +1,12 @@
-function [] = visualisation(matrix,k)
+function [] = visualisation(matrix,n, label, rubric_version, video_name)
 imagesc(matrix);
+title(strcat(label, {', '}, int2str(rubric_version), {', '}, video_name));
 colorbar;
 xlabel('Frames');
 ylabel('Labellers');
-for i=0.5:1:(k-0.5)
+for i=0.5:1:(n-0.5)
     yline(i);
 end
-
-
+saveas(gcf,label, 'png');
+disp(strcat('Saved file : ', label)) 
+end
