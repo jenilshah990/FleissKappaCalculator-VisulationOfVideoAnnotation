4c0b33ad5114b9a19e3362c9ca0c8dcf280318fb
Jenil Shah <55563927+jenilshah990@users.noreply.github.com>
2021-03-08 16:50:14 -0500

    Update heat_map.m

--- heat_map.m
+++ heat_map.m
@@ -3,15 +3,37 @@ function [] = heat_map(matrix,n, label, rubric_version, video_name)
 %Creating Heat Map
 matrix2 = transpose(matrix);
 imagesc(matrix2);
-title(strcat({'Heatmap, '}, label, {', '}, int2str(rubric_version), {', '}, video_name));
+
+%Reformats video name so _ doesn't become subscript
+v_name = '';
+i = 1;
+while i <= length(video_name)
+    if video_name(i) == '_'
+        v_name = strcat(v_name, '\_');
+        i = i + 1;
+    end
+    v_name = strcat(v_name, video_name(i));
+    i = i + 1;
+end
+
+title(strcat({'Heatmap, '}, label, {', '}, rubric_version, {', '}, v_name));
 colorbar;
 colormap(autumn);
-xlabel('Frames');
-ylabel('Categories');
+xlabel('Frames (x2 for Face, x7 for Cabin)');
+ylabel('Label Categories');
 for i=0.5:1:(n-0.5)
     yline(i);
 end
+
+yl = ylim;
+yticks(1:1:yl(2)-0.5);
+
 filename = strcat(label, '-heat');
-saveas(gcf,filename,'png');
-disp(strcat('Saved file: ', filename)) 
+%saveas(gcf,filename,'png');
+disp(strcat('Saved file: ', filename, '.png'))
+
+%Saves picture directly to predefined result directory
+filePath = strcat(video_name, "-results", '/', filename, '.png');
+saveas(gcf, [filePath]);
+
 end
