b4a1b2a291f311aaa88f2fd9e6c51bfebf03f331
Jenil Shah <55563927+jenilshah990@users.noreply.github.com>
2021-03-08 16:49:53 -0500

    Update fleiss.m

--- fleiss.m
+++ fleiss.m
@@ -21,11 +21,15 @@
 %           - Kappa benchmarks by Landis and Koch 
 %           - z test & P Value
 
-function fleiss(k, matrix, alpha, label, rubric_version, video_name)
+function fleiss(k, matrix, alpha, label, rubric_version, video_name, labelers)
 n=size(matrix, 1); %Number of Labellers
 matrix(matrix(:) == 10) = k; %Changing 10 to highest value k
-visualisation(matrix,n, label, rubric_version, video_name); 
+visualisation(matrix,n, label, rubric_version, video_name, labelers); 
 F = Create_Fleiss_Matrix(k,matrix);
-heat_map(F,k, label, rubric_version, video_name);
-fleiss_score(F,alpha);
+%figure
+temp = F;
+heat_map(temp, k, label, rubric_version, video_name);
+%figure
+fleiss_score(F,alpha, label, video_name);
+fclose('all');
 end
